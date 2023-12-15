d589612a6337307db2beb486fce767c02c6fa89b
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-12-16 00:11:12 +0500

    schema defined

--- models/userSchema.js
+++ models/userSchema.js
@@ -15,10 +15,6 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
 });
 
 module.exports = mongoose.model("users", UserSchema);
