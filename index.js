56c44ab61567d5d6a14dffca408b23b591bd83d9
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-12-18 16:17:25 +0500

    add host

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 app.use(cors())
 app.use(cors({
-  origin: 'https://card-clubb.vercel.app/'
+  origin: 'https://card-clubb-frontend.vercel.app/'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
